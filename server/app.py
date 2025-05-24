1d00619530e65097b8360e4b8da69b035ddd4af2
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-05-24 13:43:10 -0700

    Add DoctorById class to app.py

--- server/app.py
+++ server/app.py
@@ -2,8 +2,8 @@
 
 from flask import request, session, jsonify, make_response
 from flask_restful import Resource
-
 from config import app, db, api
+from models import Doctor, Appointment
 
 @app.route('/')
 def index():
@@ -19,3 +19,21 @@ class Doctors(Resource):
         return make_response(jsonify(doctors), 200)
     
 api.add_resource(Doctors, '/doctors')
+
+class DoctorById(Resource):
+    def get(doctor_id):
+        doctor = Doctor.query.get(doctor_id)
+
+        if not doctor:
+            return {'error': 'Doctor not found'}, 404
+        return make_response(jsonify(doctor.to_dict()), 200)
+    
+    def delete(doctor_id):
+        doctor = Doctor.query.get(doctor_id)
+
+        if not doctor:
+            return {'error': 'Doctor not found'}, 404
+        
+        db.session.delete(doctor)
+        db.session.commit()
+        return {}, 204
